f22b7a28600b0895db5c87ca844f68f955a0d5b6
bishwajit93 <bish.karm123@gmail.com>
2024-09-03 11:41:55 +0200

    userFOrmUpdated

--- myfirstreactapp/src/components/userform.tsx
+++ myfirstreactapp/src/components/userform.tsx
@@ -1,37 +1,46 @@
 "use client"
-
-import { useState, ChangeEvent } from "react"
+import { useState, ChangeEvent, FormEvent } from "react"
 
 type FormState = {
-    name: string;
-    age: number;
+    name: string
+    age: number | undefined
+    postcode: number | undefined
 }
 
-export default function UserForm() {
-    const [form, setForm] = useState<FormState>({name: "", age: 0})
+export default function UserForm(){
+    const [form, setForm] = useState<FormState>({name: "", age: 0, postcode: 0})
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const {name, value} = e.target
-        setForm (prevForm => ({
-            ...prevForm,
-            [name]: value
-        }))
+
+        const parsedValue = name === "age" || name == "postcode" ? Number(value) : value
+        setForm(prevForm => ({...prevForm, [name]:parsedValue}))
+    }
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        console.log('Form Submitted : ', form)
     }
     return(
         <div>
-            <form action="">
-                <label htmlFor="">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">
                     Name:
-                    <input type="text" name="name" value={form.name} onChange={handleChange}/>
+                    <input type="text" name="name" value={form.name} onChange={handleChange} />
                 </label>
-                <label htmlFor="">
+                <label htmlFor="age">
                     Age:
                     <input type="number" name="age" value={form.age} onChange={handleChange} />
                 </label>
+                <label htmlFor="postcode">
+                    Postal Code:
+                    <input type="number" name="postcode" value={form.postcode} onChange={handleChange} />
+                </label>
             </form>
-            <h3>Form Values:</h3>
+            <h1>Input Elements:</h1>
             <p>Name: {form.name}</p>
             <p>Age: {form.age}</p>
+            <p>Postal Code: {form.postcode}</p>
         </div>
     )
-}
+} 
